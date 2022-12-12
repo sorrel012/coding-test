c104324279272c0bdb5103c70ee219ee3fb7267e
sorrel012 <115568532+sorrel012@users.noreply.github.com>
2022-12-12 18:12:12 +0900

    [level 0] Title: 배열의 평균값, Time: 0.02 ms, Memory: 72.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120817. 배열의 평균값/배열의 평균값.java
+++ 프로그래머스/lv0/120817. 배열의 평균값/배열의 평균값.java
@@ -2,12 +2,10 @@ class Solution {
     public double solution(int[] numbers) {
         double answer = 0;
 		double sum = 0.0;
-		int count = 0;
 		for(int i: numbers) {
 			sum += i;
-			count += 1;
 		}
-        answer = sum / count;
+        answer = sum / numbers.length;
         return answer;
     }
 }
