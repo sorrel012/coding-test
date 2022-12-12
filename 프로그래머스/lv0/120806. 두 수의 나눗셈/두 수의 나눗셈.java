feefcb8f78d289dbf5da4210693cff642894070c
sorrel012 <115568532+sorrel012@users.noreply.github.com>
2022-12-12 17:06:50 +0900

    Update 두 수의 나눗셈.java

--- 프로그래머스/lv0/120806. 두 수의 나눗셈/두 수의 나눗셈.java
+++ 프로그래머스/lv0/120806. 두 수의 나눗셈/두 수의 나눗셈.java
@@ -1,8 +1,7 @@
 class Solution {
     public int solution(int num1, int num2) {
-        int answer = 0;
         double result = ( (double) num1 / num2 ) * 1000;
-        answer = (int) result;
+        int answer = (int) result;
         return answer;
     }
 }
