cf95430883133db1d16bdb060d87b940f5d98d9c
sorrel012 <115568532+sorrel012@users.noreply.github.com>
2022-12-12 17:05:49 +0900

    [level 0] Title: 두 수의 나눗셈, Time: 0.01 ms, Memory: 78.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120806. 두 수의 나눗셈/두 수의 나눗셈.java
+++ 프로그래머스/lv0/120806. 두 수의 나눗셈/두 수의 나눗셈.java
@@ -1,7 +1,8 @@
 class Solution {
     public int solution(int num1, int num2) {
         int answer = 0;
-        answer = (int) ( ((float) num1 / num2) * 1000 );
+        double result = ( (double) num1 / num2 ) * 1000;
+        answer = (int) result;
         return answer;
     }
 }
