5af6b34493595ba5cc0ca957ff9a876b9fc3bfbb
sorrel012 <115568532+sorrel012@users.noreply.github.com>
2022-12-20 22:25:52 +0900

    [level 0] Title: 배열 자르기, Time: 0.03 ms, Memory: 72.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120833. 배열 자르기/배열 자르기.java
+++ 프로그래머스/lv0/120833. 배열 자르기/배열 자르기.java
@@ -1,19 +1,8 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 
 class Solution {
     public int[] solution(int[] numbers, int num1, int num2) {
-        List<Integer> number = new ArrayList<>();
-        
-        for(int i = num1; i <= num2; i++) {
-            number.add(numbers[i]);
-        }
-        
-        int[] answer = new int[number.size()];
-            
-        for(int i = 0; i < number.size(); i++) {
-            answer[i] = number.get(i);
-        }
+        int[] answer = Arrays.copyOfRange(numbers, num1, num2 + 1);
         
         return answer;
     }
