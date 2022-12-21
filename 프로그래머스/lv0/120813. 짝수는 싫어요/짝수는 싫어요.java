db3d628e30d103ae0bda3a3f57449b9d88f3c16e
sorrel012 <115568532+sorrel012@users.noreply.github.com>
2022-12-21 23:44:21 +0900

    [level 0] Title: 짝수는 싫어요, Time: 3.98 ms, Memory: 73.7 MB -BaekjoonHub

--- 프로그래머스/lv0/120813. 짝수는 싫어요/짝수는 싫어요.java
+++ 프로그래머스/lv0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -1,19 +1,7 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.*;
+import java.util.stream.IntStream;
 
 class Solution {
     public int[] solution(int n) {
-        List<Integer> odd = new ArrayList<>();
-        
-        for(int i = 1; i <= n; i++) {
-            if(i % 2 == 1) {
-                odd.add(i);
-            }
-        }
-        
-        int[] answer = odd.stream().mapToInt(i->i).toArray();
-            
-        return answer;
+        return IntStream.rangeClosed(0, n).filter(value -> value % 2 == 1).toArray();
     }
 }
