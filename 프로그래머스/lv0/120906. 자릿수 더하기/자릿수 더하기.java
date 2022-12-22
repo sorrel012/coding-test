14452835e2eb8f10a89673f932ab8c19e6efe79a
sorrel012 <115568532+sorrel012@users.noreply.github.com>
2022-12-22 15:43:12 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.02 ms, Memory: 82.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120906. 자릿수 더하기/자릿수 더하기.java
+++ 프로그래머스/lv0/120906. 자릿수 더하기/자릿수 더하기.java
@@ -1,16 +1,12 @@
 class Solution {
     public int solution(int n) {
         int answer = 0;
-        String number = n + "";
-        int length = number.length();
-        
-        for(int i = length -1 ; i >= 1; i--) {
-        	answer += n/(int)Math.pow(10, i);
-            n -= (n/(int)Math.pow(10, i)) * ((int)Math.pow(10, i));
+
+        while(n > 0){
+            answer += n % 10;
+            n /= 10;
         }
-        
-        answer += n;
-        
+
         return answer;
     }
 }
