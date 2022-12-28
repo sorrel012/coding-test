e83870998b336c8a587da5a4c97565da89e58d9d
sorrel012 <115568532+sorrel012@users.noreply.github.com>
2022-12-28 23:51:32 +0900

    [level 0] Title: 숫자 찾기, Time: 6.00 ms, Memory: 67.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120904. 숫자 찾기/숫자 찾기.java
+++ 프로그래머스/lv0/120904. 숫자 찾기/숫자 찾기.java
@@ -1,14 +1,5 @@
 class Solution {
-    public int solution(int num, int k) {        
-        String number = String.valueOf(num);        
-        int answer = 0;
-        
-        if(number.indexOf((k+'0')) == -1) {
-            answer = -1;
-        } else {
-            answer = number.indexOf((k+'0')) + 1;
-        }
-                
-        return answer;
+    public int solution(int num, int k) {     
+        return ("-" + num).indexOf(String.valueOf(k));
     }
 }
