2ae82c28d1eb8255312ab234e46df301985c6815
sorrel012 <115568532+sorrel012@users.noreply.github.com>
2022-12-29 17:21:14 +0900

    [level 0] Title: 합성수 찾기, Time: 0.01 ms, Memory: 78.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120846. 합성수 찾기/합성수 찾기.java
+++ 프로그래머스/lv0/120846. 합성수 찾기/합성수 찾기.java
@@ -1,28 +1,15 @@
 class Solution {
-    
     public int solution(int n) {
         int answer = 0;
-        
-        for(int i=2; i<=n; i++) {
-            if(compositeNumber(i)) {
-                answer += 1;
+
+        for (int i = 1; i <= n; i++) {
+            int cnt = 0;
+            for (int j = 1; j <= i; j++) {
+                if (i % j == 0) { cnt++; }
             }
+            if (cnt >= 3) { answer++; }
         }
-        
+
         return answer;
     }
-    
-    public boolean compositeNumber(int n) {
-        boolean isComposite = false;
-        if(n > 1) {
-            for(int i=2; i < n; i++) {
-                if (n % i == 0) {
-                    isComposite = true;
-                    break;
-                } //if
-            } //for
-        }//if
-        return isComposite;
-    }//compositeNumber()
-    
 }
