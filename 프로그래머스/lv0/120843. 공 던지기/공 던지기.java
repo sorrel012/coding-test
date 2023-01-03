23e02705afaa9585e2be3b14d392d195863b9461
sorrel012 <115568532+sorrel012@users.noreply.github.com>
2023-01-03 18:45:13 +0900

    [level 0] Title: 공 던지기, Time: 0.02 ms, Memory: 69.9 MB -BaekjoonHub

--- 프로그래머스/lv0/120843. 공 던지기/공 던지기.java
+++ 프로그래머스/lv0/120843. 공 던지기/공 던지기.java
@@ -1,22 +1,5 @@
 class Solution {
-    public int solution(int[] numbers, int k) {
-        int answer = 0;
-		int cnt = 0;
-        
-        for(int i=0; i<k*2; i=i+2) {
-        	cnt += 1;
-            if(i > numbers.length-1) {
-            	i = i - numbers.length;
-                answer = numbers[i];
-            }
-            else
-                answer = numbers[i];
-            
-            if(cnt == k) 
-            	break;          
-         
-        }
-        
-        return answer;
+    public int solution(int[] numbers, int k) {    
+        return numbers[(k-1)*2 % numbers.length];
     }
 }
