7bca06dc7c205791df4f79f33c058c2beb339b35
sorrel012 <115568532+sorrel012@users.noreply.github.com>
2023-01-16 22:17:40 +0900

    [level 0] Title: 문자열 밀기, Time: 0.02 ms, Memory: 79 MB -BaekjoonHub

--- 프로그래머스/lv0/120921. 문자열 밀기/문자열 밀기.java
+++ 프로그래머스/lv0/120921. 문자열 밀기/문자열 밀기.java
@@ -1,32 +1,6 @@
-import static java.util.Collections.rotate;
-
-import java.util.ArrayList;
-import java.util.List;
-
 class Solution {
     public int solution(String A, String B) {
-       List<Character> stringA = new ArrayList<>();
-		List<Character> stringB = new ArrayList<>();
-
-		for (int i = 0; i < A.length(); i++) {
-			stringA.add(A.charAt(i));
-			stringB.add(B.charAt(i));
-		}
-
-		int answer = -1;
-
-		if (stringA.equals(stringB))
-			answer = 0;
-
-		for (int i = 0; i < stringA.size() - 1; i++) {
-			rotate(stringA, 1);
-			System.out.println("stringA = " + stringA);
-			if (stringA.equals(stringB)) {
-				answer = i + 1;
-				break;
-			}
-		}
-        
-        return answer;
+        String tmpB = B.repeat(2);
+        return tmpB.indexOf(A);
     }
 }
