4bbed81ae272651e79e44acd835ca19aac1ae8a4
hyowonhan <wain1719@gmail.com>
2023-02-21 21:41:26 +0900

    refactor: use enhanced for each

--- algorithm/src/com/sorrel012/string/Q01.java
+++ algorithm/src/com/sorrel012/string/Q01.java
@@ -11,12 +11,10 @@ public class Q01 {
 		str = str.toUpperCase();
 		t = Character.toUpperCase(t);
 		
-		for(int i = 0; i < str.length(); i++) {
-			
-			if(str.charAt(i) == t) {
+		for(char x : str.toCharArray()) {
+			if(x == t) {
 				answer++;
 			}
-			
 		}
 		
 		return answer;
