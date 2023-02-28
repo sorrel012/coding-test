52335f532e25587ea465099c266c9a50f1eef9f4
hwh <sorrel012@gmail.com>
2023-02-28 22:01:35 +0900

    feat: covert symbol to char

---
+++ algorithm/src/com/sorrel012/string/Q12.java
@@ -0,0 +1,45 @@
+package com.sorrel012.string;
+
+import java.util.Scanner;
+
+public class Q12 {
+
+	private String solution(int count, String str) {
+		
+		String answer = "";
+		
+		str = str.replace("#", "1").replace("*", "0");
+
+		int[] bin = new int[count];
+		
+		for(int i = 0; i < count; i++) {
+			
+			String tmp = str.substring(i*7, i*7+7);
+			
+			bin[i] = Integer.parseInt(tmp, 2);
+			
+		}
+		
+		for(int i : bin) {
+			answer += (char)i;
+		}
+		
+		return answer;
+	}
+
+	public static void main(String[] args) {
+
+		Q12 t = new Q12();
+		
+		Scanner scan = new Scanner(System.in);
+		
+		int count = scan.nextInt();
+		scan.nextLine();
+		
+		String str = scan.nextLine();
+		
+		System.out.print(t.solution(count, str));
+
+	} //main
+	
+}
