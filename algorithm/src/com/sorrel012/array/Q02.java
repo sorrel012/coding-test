4de28ab5a92cefa9ea883097b4efb98522ce8e9f
hwh <sorrel012@gmail.com>
2023-03-01 18:09:12 +0900

    feat: print the number of students seen in the front

--- algorithm/src/com/sorrel012/array/Q02.java
+++ algorithm/src/com/sorrel012/array/Q02.java
@@ -4,58 +4,23 @@ import java.util.Scanner;
 
 public class Q02 {
 	
-	private String[] solution(int[] a, int[] b) {
+	private int solution(int[] nums) {
 		
-		String[] answer = new String[a.length];
+		int answer = 1;
 		
-		for(int i = 0; i < a.length; i++) {
+		int tmp = nums[0];
+		
+		for(int i = 1; i < nums.length; i++) {
 			
-			if(a[i] == 1) {
-				
-				switch (b[i]) {
-				case 1 :
-					answer[i] = "D";
-					break;
-				case 2 :
-					answer[i] = "B";
-					break;
-				case 3 :
-					answer[i] = "A";
-					break;
-				}
-				
-			} else if(a[i] == 2) {
-				
-				switch (b[i]) {
-				case 1 :
-					answer[i] = "A";
-					break;
-				case 2 :
-					answer[i] = "D";
-					break;
-				case 3 :
-					answer[i] = "B";
-					break;
-				}
-				
-			} else {
-				
-				switch (b[i]) {
-				case 1 :
-					answer[i] = "B";
-					break;
-				case 2 :
-					answer[i] = "A";
-					break;
-				case 3 :
-					answer[i] = "D";
-					break;
-				}
-				
+			if(nums[i] > tmp) {
+				answer++;
+				tmp = nums[i];
 			}
+			
 		}
-				
+		
 		return answer;
+		
 	}
 	
 	public static void main(String[] args) {
@@ -66,20 +31,13 @@ public class Q02 {
 		
 		int count = scan.nextInt();
 		
-		int[] a = new int[count];
-		int[] b = new int[count];
+		int[] nums = new int[count];
 		
 		for(int i = 0; i < count; i++) {
-			a[i] = scan.nextInt();
+			nums[i] = scan.nextInt();
 		}
 		
-		for(int i = 0; i < count; i++) {
-			b[i] = scan.nextInt();
-		}
-		
-		for(String str : t.solution(a, b)) {
-			System.out.println(str);
-		}
+		System.out.println(t.solution(nums));
 
 	} //main
 	
