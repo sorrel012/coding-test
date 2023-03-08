aa9251e9cbe049188269c74b0e26729438099ef7
hwh <sorrel012@gmail.com>
2023-03-09 00:07:35 +0900

    feat: Implement Memtoring system using two-dimensional List

--- algorithm/src/com/sorrel012/array/Q12.java
+++ algorithm/src/com/sorrel012/array/Q12.java
@@ -1,5 +1,7 @@
 package com.sorrel012.array;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Q12 {
@@ -13,23 +15,50 @@ public class Q12 {
 		int n = scan.nextInt();
 		int m  = scan.nextInt();
 		
-		int[][] students = new int[m][n];
+		List<List<Integer>> students = new ArrayList<List<Integer>>();
 
+		
 		for(int i = 0; i < m; i++) {
+			List<Integer> tmp = new ArrayList<Integer>();
+			
 			for(int j = 0; j < n; j++) {
-				students[i][j] = scan.nextInt();
+				tmp.add(scan.nextInt());
 			}
+			
+			students.add(tmp);
 		}
 		
-		System.out.print(t.solution(n, students));
+		System.out.print(t.solution(m, n, students));
 
 	} //main
 
-	private int solution(int n, int[][] students) {
+	private int solution(int m, int n, List<List<Integer>> students) {
 		
 		int answer = 0;
 		
-		
+		for(int i = 1; i <= n; i++) {		//학생1
+			
+			for(int j = 1; j <= n; j++) {	//학생2
+				
+				int count = 0;
+				for(int k = 0; k < m; k++) {//테스트
+					
+					int s1 = students.get(k).indexOf(i);
+					int s2 = students.get(k).indexOf(j);
+					
+					if(s1 < s2) {
+						count++;
+					}
+					
+				}
+				
+				if(count == m) {
+					answer++;
+				}
+				
+			}
+			
+		}
 		
 		return answer;
 	}
