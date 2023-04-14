08dbd07f94c04448f71ac8c10f8dc084e434b171
sorrel012 <wain1719@gmail.com>
2023-04-14 23:55:25 +0900

    feat: review python if statement

---
+++ 알고리즘/src/com/sorrel012/python/syntax/conditional-statement.py
@@ -0,0 +1,32 @@
+""" x=8
+if x==7:
+    print("Lucky")
+else:
+    print("TT")
+    
+if x>=10:
+    if x%2==1:
+        print("10 이상의 홀수")
+    else:
+        print("10 이상의 짝수")
+else:
+    if x%2==1:
+        print("10 미만의 홀수")
+    else:
+        print("10 미만의 짝수") """
+
+""" x=7
+if x>=0 and x<10:
+    print("10보다 작은 자연수") """
+    
+x=93
+if x>=90:
+    print('A')
+elif x>=80:
+    print('B')
+elif x>=70:
+    print('C')
+elif x>=60:
+    print('D')
+else:
+    print('F')  
