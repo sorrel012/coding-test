530ae96a3222ce32f48d6b5acb36567d4d52d97f
sorrel012 <wain1719@gmail.com>
2023-04-15 13:45:13 +0900

    refactor: use break and for-else instead of list

--- 알고리즘/src/com/sorrel012/python/basic/Q01.py
+++ 알고리즘/src/com/sorrel012/python/basic/Q01.py
@@ -1,12 +1,14 @@
 n, k = map(int, input().split())
 
-a = []
+cnt = 0
 
 for i in range(1, n+1):
-    if (n % i == 0):
-        a.append(i)
-
-if(len(a) >= k):
-    print(a[k-1])
+    
+    if n%i == 0:
+        cnt += 1
+        
+    if cnt == k:
+        print(i)
+        break
 else:
     print(-1)
