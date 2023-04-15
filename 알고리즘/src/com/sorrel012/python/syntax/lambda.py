792506a8c67192d4ff0512f4c26ee1b6daf2481b
sorrel012 <wain1719@gmail.com>
2023-04-15 13:16:12 +0900

    feat: review python lambda expression

---
+++ 알고리즘/src/com/sorrel012/python/syntax/lambda.py
@@ -0,0 +1,11 @@
+def plus_one(x):
+    return x+1
+
+# print(plus_one(1))
+
+# plus_two = lambda x: x+2
+# print(plus_two(1))
+
+a = [1, 2, 3]
+print(list(map(plus_one, a)))
+print(list(map(lambda x: x+1, a)))
