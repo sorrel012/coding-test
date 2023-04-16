c7c7a52333b35fb479070a67f31309e99baba16e
sorrel012 <wain1719@gmail.com>
2023-04-16 14:37:59 +0900

    refactor: use set to remove duplicate numbers

--- 알고리즘/src/com/sorrel012/python/basic/Q03.py
+++ 알고리즘/src/com/sorrel012/python/basic/Q03.py
@@ -1,15 +1,15 @@
 n, k = map(int, input().split())
 li = list(map(int, input().split()))
 
-sum_li = []
+res = set()
 
 for i in range(n):
     for j in range(i+1, n):
         for l in range(j+1, n):
             sum = li[i] + li[j] + li[l]
-            if(sum_li.count(sum) == 0) :
-                sum_li.append(sum)
+            res.add(sum)
             
-sum_li.sort(reverse=True)
+res = list(res)            
+res.sort(reverse=True)
 
-print(sum_li[k-1])
+print(res[k-1])
