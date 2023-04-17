70340e4c80a75eb2d014bd9b88ab7b61180bd774
sorrel012 <wain1719@gmail.com>
2023-04-17 23:01:39 +0900

    refactor: use format specifier instead of end=''

--- 알고리즘/src/com/sorrel012/python/basic/Q02.py
+++ 알고리즘/src/com/sorrel012/python/basic/Q02.py
@@ -3,10 +3,8 @@ t = int(input())
 for i in range(1, t+1):
     n, s, e, k = map(int, input().split())
     a = list(map(int, input().split()))
-    slice_a = a[s-1:e]
-    slice_a.sort()
-    res = slice_a[k-1]
+    a = a[s-1:e]
+    a.sort()
+    res = a[k-1]
     
-    print("#", end='')
-    print(i, res)
-    print()
+    print("#%d %d" %(i, res))
