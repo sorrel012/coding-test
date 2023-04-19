5015a5f81631d4538469722eff292d0cb675604b
sorrel012 <wain1719@gmail.com>
2023-04-19 22:38:10 +0900

    refactor: count the number of sums instead of using extra list to store sum

--- 알고리즘/src/com/sorrel012/python/basic/Q5.py
+++ 알고리즘/src/com/sorrel012/python/basic/Q5.py
@@ -1,26 +1,16 @@
 n, m = map(int, input().split())
- 
-sum = []
- 
+
+cnt = [0]*(n+m+1)
+max = -2147000000
+
 for i in range(1, n+1):
     for j in range(1, m+1):
-        sum.append(i+j)
-
-res = set()
-max = -1
-
-for x in sum:
-    if sum.count(x) > max:
-        max = sum.count(x)
-
-
-for x in sum:
-    if sum.count(x) == max :
-        res.add(x)
-
+        cnt[i+j] += 1
         
-res = list(res)
-res.sort()
-
-for x in res:
-    print(x, end=' ')
+for i in range(n+m+1):
+    if cnt[i] > max:
+        max = cnt[i]
+        
+for i in range(n+m+1):
+    if cnt[i] == max:
+        print(i, end=' ')    
