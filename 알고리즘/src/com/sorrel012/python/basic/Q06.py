f1124d5b6afdf3546b88ad183fc1675ade870ac3
sorrel012 <wain1719@gmail.com>
2023-04-20 22:45:24 +0900

    refactor: iterate String instead of while

--- 알고리즘/src/com/sorrel012/python/basic/Q06.py
+++ 알고리즘/src/com/sorrel012/python/basic/Q06.py
@@ -1,11 +1,17 @@
 n = int(input())
 a = list(map(int, input().split()))
 
+# def digit_sum(x):
+#     sum = 0
+#     while(x > 0):
+#         sum += x%10
+#         x = x//10
+#     return sum
+
 def digit_sum(x):
     sum = 0
-    while(x > 0):
-        sum += x%10
-        x = x//10
+    for i in str(x):
+        sum += int(i)
     return sum
         
 max = -2147000000
