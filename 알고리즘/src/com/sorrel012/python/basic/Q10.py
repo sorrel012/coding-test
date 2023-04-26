9f0be02481da733c61d14211b050c43e7b67b373
sorrel012 <wain1719@gmail.com>
2023-04-26 22:36:46 +0900

    feat: calculate continous scores

---
+++ 알고리즘/src/com/sorrel012/python/basic/Q10.py
@@ -0,0 +1,15 @@
+n = int(input())
+a = list(map(int, input().split()))
+
+cnt = 0
+score = 0
+
+for x in a:
+    if x == 0:
+        cnt = 0
+    else :
+        cnt += 1
+        score += cnt
+    print(x, cnt, score)
+    
+print(score)
