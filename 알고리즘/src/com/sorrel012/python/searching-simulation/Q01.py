bdf6e6d5654928ba3406ffc463e8b4c3a7c2e990
sorrel012 <wain1719@gmail.com>
2023-04-27 22:35:06 +0900

    refactor: use array[::] instead of for loop

--- 알고리즘/src/com/sorrel012/python/searching-simulation/Q01.py
+++ 알고리즘/src/com/sorrel012/python/searching-simulation/Q01.py
@@ -1,13 +1,18 @@
 n = int(input())
 
+# for i in range(1, n+1):
+#     str = input().lower()
+#     reStr = ""
+#     for c in str:
+#         reStr = c + reStr
+#     if(str == reStr):
+#         print("#%d YES" %i)
+#     else:
+#         print("#%d NO" %i)
+
 for i in range(1, n+1):
-    str = input().lower()
-    reStr = ""
-    for c in str:
-        reStr = c + reStr
-    if(str == reStr):
+    s = input().lower()
+    if s == s[::-1]:
         print("#%d YES" %i)
-    else:
+    else :
         print("#%d NO" %i)
-  
-    
