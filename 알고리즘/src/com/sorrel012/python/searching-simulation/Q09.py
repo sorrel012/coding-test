5e24b954dd4ebb6e9b92027893ff8db99bec49c0
sorrel012 <sorrel012@gmail.com>
2023-05-06 18:36:58 +0900

    feat: Get number  which is bigger than numbers that surrounding

---
+++ 알고리즘/src/com/sorrel012/python/searching-simulation/Q09.py
@@ -0,0 +1,18 @@
+n = int(input())
+a = [list(map(int, input().split())) for _ in range(n)]
+
+for i in range(n):
+    a[i].insert(0, 0)
+    a[i].append(0)    
+
+a.insert(0, [0]*(n+2))
+a.append([0]*(n+2))
+
+cnt = 0
+
+for i in range(1, n+1):
+    for j in range(1, n+1):
+        if (a[i][j] > a[i-1][j]) & (a[i][j] > a[i+1][j]) & (a[i][j] > a[i][j-1]) & (a[i][j] > a[i][j+1]):
+            cnt +=1 
+            
+print(cnt)
