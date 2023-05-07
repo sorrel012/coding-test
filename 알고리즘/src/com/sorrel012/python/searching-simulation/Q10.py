8cad078ead382193deb2f1a8e64dce5f8a24600a
sorrel012 <wain1719@gmail.com>
2023-05-07 17:09:45 +0900

    refactor: Extract method

--- 알고리즘/src/com/sorrel012/python/searching-simulation/Q10.py
+++ 알고리즘/src/com/sorrel012/python/searching-simulation/Q10.py
@@ -1,30 +1,28 @@
-a = [list(map(int, input().split())) for _ in range(9)]
-
-
-chGroup = [0]*10
-
-res = "YES"
-
-for i in range(9):
-    chRow = [0]*10
-    chCol = [0]*10
-    for j in range(9):
-        chRow[a[i][j]] = 1
-        chCol[a[j][i]] = 1
-    if sum(chRow) != 9 or sum(chCol) != 9:
-        res = "NO"
-        break
+def check(a):
+    for i in range(9):
+        chRow = [0]*10
+        chCol = [0]*10
+        for j in range(9):
+            chRow[a[i][j]] = 1
+            chCol[a[j][i]] = 1
+        if sum(chRow) != 9 or sum(chCol) != 9:
+            return False
     
-dx = [-1,0,1,-1,1,-1,0,1,0]
-dy = [1,1,1,0,0,-1,-1,-1,0]
-
-if res == "YES" :
+    dx = [-1,0,1,-1,1,-1,0,1,0]
+    dy = [1,1,1,0,0,-1,-1,-1,0]
+    chGroup = [0]*10
     for i in range(1, 8, 3):
         for j in range(1, 8, 3):
             for k in range(9):
                 chGroup[a[i+dy[k]][j+dx[k]]] = 1
             if sum(chGroup) != 9:
-                res = "NO"
-                break
+                return False
+    
+    return True
             
-print(res)
+a = [list(map(int, input().split())) for _ in range(9)]
+
+if check(a):
+    print("YES")
+else:
+    print("NO")
