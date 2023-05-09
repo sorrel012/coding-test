f6c7c88ddb7f1b1252fad8e44b529851f70fb573
sorrel012 <wain1719@gmail.com>
2023-05-09 22:11:28 +0900

    refactor: Use binary search

--- 알고리즘/src/com/sorrel012/python/greedy/Q01.py
+++ 알고리즘/src/com/sorrel012/python/greedy/Q01.py
@@ -3,4 +3,14 @@ a = list(map(int, input().split()))
 
 a.sort()
 
-print(a.index(m)+1)
+lt = 0
+rt = n-1
+while lt <= rt:
+    mid = (lt+rt) //2
+    if a[mid] == m:
+        print(mid+1)
+        break
+    elif a[mid] > m:
+        rt = mid-1
+    else:
+        lt = mid+1
