b907cbe0d166f0c7aafcf6cab7214231da13c02e
sorrel012 <wain1719@gmail.com>
2023-05-12 21:58:16 +0900

    feat: Print minimum capacity of DVD

---
+++ 알고리즘/src/com/sorrel012/python/greedy/Q03.py
@@ -0,0 +1,28 @@
+n, m = map(int, input().split())
+a = list(map(int, input().split()))
+
+def Count(capacity):
+    cnt = 1
+    sum = 0
+    for x in a:
+        if sum+x > capacity:
+           cnt +=1 
+           sum = x 
+        else: 
+            sum += x 
+    
+    return cnt
+    
+lt = 1
+rt = sum(a) 
+
+while lt <= rt:
+    mid = (lt + rt) //2
+    
+    if Count(mid) <= m:
+        res = mid 
+        rt = mid -1
+        
+    else: lt = mid +1
+    
+print(res)
