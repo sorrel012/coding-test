360af4ddd63334dc664cd7d87b7e9ed1cd79d1e2
sorrel012 <wain1719@gmail.com>
2023-05-13 15:03:50 +0900

    feat: Get maximum distance of the horses that the distance of thoses horses is the nearest.

--- 알고리즘/src/com/sorrel012/python/greedy/Q04.py
+++ 알고리즘/src/com/sorrel012/python/greedy/Q04.py
@@ -2,4 +2,32 @@ n, c = map(int, input().split())
 a = []
 
 for _ in range(n):
-    a.append(int(input().split()))
+    a.append(int(input()))
+
+a.sort()
+
+lt = 1
+rt = a[n-1]
+
+def Count(minLen):
+    cnt = 1
+    ep = a[0]
+    
+    for i in range(1, n):
+        if a[i]-ep >= minLen:
+            cnt +=1
+            ep = a[i]
+    
+    return cnt
+    
+res = 0
+while lt <= rt:
+    minLen = (lt+rt) // 2
+    
+    if Count(minLen) >= c:
+        res = minLen
+        lt = minLen + 1
+    else :
+        rt = minLen - 1
+
+print(res)
