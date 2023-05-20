51b6fc37057e24bca7a44283aca6baaf3aaae87a
sorrel012 <wain1719@gmail.com>
2023-05-20 15:45:27 +0900

    refactor: Use deque instead of list

--- 알고리즘/src/com/sorrel012/python/data-structure/Q05.py
+++ 알고리즘/src/com/sorrel012/python/data-structure/Q05.py
@@ -1,14 +1,15 @@
+from collections import deque
+
 n, k = map(int, input().split())
 
-queue = [i for i in range(1, n+1)]
+dq = list(range(1, n+1))
+dq = deque(dq)
 
-cnt = 1
-while len(queue) > 1:    
-    if(cnt == k):
-        queue.pop(0)
-        cnt = 1
-    else:
-        queue.append(queue.pop(0))
-        cnt += 1
+while len(dq) > 1:  
+    for _ in range(k-1):
+        cur = dq.popleft()
+        dq.append(cur)
+    
+    dq.popleft()
         
-print(queue[0])
+print(dq[0])
