f68af6f37ee91f1275201f0f3f66f43d6cea81c6
sorrel012 <115568532+sorrel012@users.noreply.github.com>
2023-06-28 22:29:04 +0900

    [Bronze II] Title: 소수 찾기, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/1978. 소수 찾기/소수 찾기.py
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.py
@@ -3,15 +3,14 @@ a = list(map(int, input().split()))
 
 cnt = 0
 
-for i in range(n):
-    num = a[i]
-
-    tmp = 0
-    for j in range(1, num+1):
-        if num%j == 0 :
-            tmp += 1
-
-    if tmp == 2:
+for i in a:
+    if i == 1:
+        continue
+        
+    for j in range(2, int(i**0.5)+1):
+        if i % j == 0:
+            break
+    else:
         cnt += 1
                 
 print(cnt)
