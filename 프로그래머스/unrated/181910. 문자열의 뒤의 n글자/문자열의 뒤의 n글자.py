3d7313d1859516f2e57d7466f8543c40c82ce005
sorrel012 <115568532+sorrel012@users.noreply.github.com>
2023-07-15 00:13:02 +0900

    [unrated] Title: 문자열의 뒤의 n글자, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181910. 문자열의 뒤의 n글자/문자열의 뒤의 n글자.py
+++ 프로그래머스/unrated/181910. 문자열의 뒤의 n글자/문자열의 뒤의 n글자.py
@@ -1,9 +1,2 @@
 def solution(my_string, n):
-    answer = ''
-    
-    str_len = len(my_string)
-    
-    for i in range(str_len - 1, str_len - n - 1, -1):
-        answer = my_string[i] + answer    
-    
-    return answer
+    return my_string[-n:]
