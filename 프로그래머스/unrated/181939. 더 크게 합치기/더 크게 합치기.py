bf9409b799a199a7248e1247d02961c6fa88d395
sorrel012 <115568532+sorrel012@users.noreply.github.com>
2023-07-15 15:21:41 +0900

    [unrated] Title: 더 크게 합치기, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181939. 더 크게 합치기/더 크게 합치기.py
+++ 프로그래머스/unrated/181939. 더 크게 합치기/더 크게 합치기.py
@@ -1,8 +1,2 @@
 def solution(a, b):
-    res1 = int(str(a)+str(b))
-    res2 = int(str(b)+str(a))
-    
-    if res1 > res2 :
-        return res1
-    else :
-        return res2
+    return int(max(f"{a}{b}", f"{b}{a}"))
