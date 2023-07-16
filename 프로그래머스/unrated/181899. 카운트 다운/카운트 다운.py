929c21f637f83f3d1607f4cf818b2d99a16a07f6
sorrel012 <115568532+sorrel012@users.noreply.github.com>
2023-07-16 18:40:19 +0900

    [unrated] Title: 카운트 다운, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181899. 카운트 다운/카운트 다운.py
+++ 프로그래머스/unrated/181899. 카운트 다운/카운트 다운.py
@@ -1,7 +1,2 @@
 def solution(start, end):
-    answer = []
-    
-    for i in range(start, end-1, -1):
-        answer.append(i)
-        
-    return answer
+    return [i for i in range(start, end-1, -1)]
