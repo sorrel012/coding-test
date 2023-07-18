2c94b520cdab25ae742a42dc4f3b859a6cd4a2ca
sorrel012 <115568532+sorrel012@users.noreply.github.com>
2023-07-18 12:32:34 +0900

    [unrated] Title: 수 조작하기 1, Time: 1.19 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181926. 수 조작하기 1/수 조작하기 1.py
+++ 프로그래머스/unrated/181926. 수 조작하기 1/수 조작하기 1.py
@@ -1,13 +1,8 @@
 def solution(n, control):
     
-    for c in control:
-        if c == 'w':
-            n += 1
-        elif c == 's':
-            n -= 1
-        elif c == 'd':
-            n += 10
-        else:
-            n -= 10 
+    c = { 'w':1, 's':-1, 'd':10, 'a':-10}
     
+    for i in control:
+        n += c[i]
+        
     return n
