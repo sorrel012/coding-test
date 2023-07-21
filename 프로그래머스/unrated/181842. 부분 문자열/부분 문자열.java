883cf4e132586585f5c4b0be26adc5d58ead1d6b
sorrel012 <115568532+sorrel012@users.noreply.github.com>
2023-07-21 23:41:30 +0900

    [unrated] Title: 부분 문자열, Time: 0.02 ms, Memory: 77.7 MB -BaekjoonHub

--- 프로그래머스/unrated/181842. 부분 문자열/부분 문자열.java
+++ 프로그래머스/unrated/181842. 부분 문자열/부분 문자열.java
@@ -1,11 +1,6 @@
 class Solution {
     public int solution(String str1, String str2) {
         
-        if (str2.indexOf(str1) == -1) {
-            return 0;
-        } else {
-            return 1;
-        }
-        
+        return str2.contains(str1) ? 1 : 0;        
     }
 }
