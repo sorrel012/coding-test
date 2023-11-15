0e23bf8ccfecf7e3c42097864f526d9c0a9e8919
Hana <115568532+sorrel012@users.noreply.github.com>
2023-11-15 17:14:36 +0900

    [level unrated] Title: A 강조하기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181874. A 강조하기/A 강조하기.js
@@ -0,0 +1,7 @@
+function solution(myString) {
+    myString = myString.toLowerCase();
+    while(myString.indexOf('a') !== -1) {
+        myString = myString.replace('a', 'A');
+    }
+    return myString;
+}
