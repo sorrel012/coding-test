31e872db95305bac1d1e6844c23ebeb185507adc
Hana <115568532+sorrel012@users.noreply.github.com>
2023-12-06 16:00:50 +0900

    [level unrated] Title: 가까운 1 찾기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181898. 가까운 1 찾기/가까운 1 찾기.js
+++ 프로그래머스/unrated/181898. 가까운 1 찾기/가까운 1 찾기.js
@@ -1,8 +1,3 @@
 function solution(arr, idx) {
-    for(let i = idx; i < arr.length; i++) {
-        if(arr[i] === 1) {
-            return i;
-        } 
-    }
-    return -1;
+  return arr.findIndex((v, i) => i >= idx && v === 1);
 }
