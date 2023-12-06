a1d95cd4417352c2ad956939262bc80db6c45099
Hana <115568532+sorrel012@users.noreply.github.com>
2023-12-06 15:31:02 +0900

    [level unrated] Title: 문자열 바꿔서 찾기, Time: 0.05 ms, Memory: 33.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.js
@@ -0,0 +1,11 @@
+function solution(myString, pat) {
+    let answer = '';
+    for(let char of myString) {
+        if (char === 'A') {
+            answer += 'B';
+        } else {
+            answer += 'A'
+        }
+    }
+    return answer.indexOf(pat) !== -1 ? 1 : 0;
+}
