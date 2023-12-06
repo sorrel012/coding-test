64fe259659fcfd33106cfb331a54f7cc7e3a7c0a
Hana <115568532+sorrel012@users.noreply.github.com>
2023-12-06 16:11:42 +0900

    [level unrated] Title: 콜라츠 수열 만들기, Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.js
+++ 프로그래머스/unrated/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.js
@@ -1,12 +1,6 @@
-function solution(n) {
-    let answer = [n];    
-    while(n != 1) {
-        if(n % 2 === 0) {            
-            n = parseInt(n/2);
-        } else {
-            n = 3 * n + 1;            
-        }
-        answer.push(n);
-    }
-    return answer;
+function solution(n, arr = []) {
+    arr.push(n);
+    if (n === 1) return arr;
+    if (n % 2 === 0) return solution(n / 2, arr);
+    return solution(3 * n + 1, arr);
 }
