bf90756c680a2cc1ed3ef2e9cad25d546d46183c
Hana <115568532+sorrel012@users.noreply.github.com>
2023-12-08 13:00:41 +0900

    [level unrated] Title: 배열 만들기 3, Time: 0.49 ms, Memory: 37.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181895. 배열 만들기 3/배열 만들기 3.js
+++ 프로그래머스/unrated/181895. 배열 만들기 3/배열 만들기 3.js
@@ -1,3 +1,4 @@
 function solution(arr, intervals) {
-    return arr.slice(intervals[0][0], intervals[0][1]+1).concat(arr.slice(intervals[1][0], intervals[1][1]+1));
+    const [[a,b],[c,d]] = intervals;
+    return [...arr.slice(a, b+1), ...arr.slice(c, d+1)];
 }
