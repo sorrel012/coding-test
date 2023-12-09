bcff63ce01f161e2c4f8918f107d58087bd1cca2
Hana <115568532+sorrel012@users.noreply.github.com>
2023-12-09 15:49:45 +0900

    [level unrated] Title: 접미사 배열, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181909. 접미사 배열/접미사 배열.js
+++ 프로그래머스/unrated/181909. 접미사 배열/접미사 배열.js
@@ -1,7 +1,7 @@
 function solution(my_string) {
     let answer = [];
     const lastIndex = my_string.length - 1;
-    for (let i = lastIndex; i >= 0; i--) {
+    for (let i = 0; i < lastIndex + 1; i++) {
         answer.push(my_string.slice(i, lastIndex + 1));
     }
     return answer.sort();
