42c250c3789794cd289f4850d42660450f365e71
Hana <115568532+sorrel012@users.noreply.github.com>
2023-12-12 10:24:31 +0900

    [level unrated] Title: 주사위 게임 2, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181930. 주사위 게임 2/주사위 게임 2.js
+++ 프로그래머스/unrated/181930. 주사위 게임 2/주사위 게임 2.js
@@ -1,11 +1,14 @@
 function solution(a, b, c) {
     const nums = new Set([a, b, c]);
-    const arr = [...nums];
-    if(arr.length === 3) {
+    if(nums.size === 3) {
         return a + b + c;
-    } else if(arr.length === 2) {
-        return (a + b + c) * (Math.pow(a, 2)+Math.pow(b, 2)+Math.pow(c, 2))
+    } else if(nums.size === 2) {
+        return (a + b + c) * calculate(a, b, c, 2)
     } else {
-        return (a + b + c) * (Math.pow(a, 2)+Math.pow(b, 2)+Math.pow(c, 2)) * (Math.pow(a, 3)+Math.pow(b, 3)+Math.pow(c, 3))
+        return (a + b + c) * calculate(a, b, c, 2) * calculate(a, b, c, 3)
     }    
+}
+
+function calculate(a, b, c, exponent) {
+    return Math.pow(a, exponent) + Math.pow(b, exponent) + Math.pow(c, exponent)
 }
