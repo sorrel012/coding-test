d50ba2e42856b32ab2bf62e12c88955f14b629ac
Hana <115568532+sorrel012@users.noreply.github.com>
2023-12-12 13:45:12 +0900

    [level unrated] Title: 세로 읽기, Time: 0.12 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181904. 세로 읽기/세로 읽기.js
+++ 프로그래머스/unrated/181904. 세로 읽기/세로 읽기.js
@@ -1,12 +1,3 @@
 function solution(my_string, m, c) {
-    let answer = '';
-    let nums = [];
-    for(let i = 0; i < my_string.length; i+= m) {
-        let tmp =  [...my_string.slice(i, i + m)];
-        nums.push(tmp);
-    }
-    for(let arr of nums) {
-        answer += arr[c-1];
-    }
-    return answer;
+    return [...my_string].filter((_, i) => i % m === c - 1).join('');
 }
