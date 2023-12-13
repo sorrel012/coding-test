a6cbd3335533f22b21b9761996fed1894d45c7e1
Hana <115568532+sorrel012@users.noreply.github.com>
2023-12-13 14:08:09 +0900

    [level unrated] Title: 날짜 비교하기, Time: 0.06 ms, Memory: 33.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181838. 날짜 비교하기/날짜 비교하기.js
@@ -0,0 +1,16 @@
+function solution(date1, date2) {
+    [year1, month1, day1] = date1;
+    [year2, month2, day2] = date2;
+    
+    if(year1 < year2) { 
+       return 1;
+    } else if (year1 === year2 && month1 < month2) {
+       return 1; 
+    } else if (month1 === month2 && day1 < day2) {
+        return 1;
+    } else {
+        return 0;
+    }
+    
+    return answer;
+}
