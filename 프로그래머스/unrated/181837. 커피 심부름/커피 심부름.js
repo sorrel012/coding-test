927550e62b437ca2567df437ca30ca9c7deab47e
Hana <115568532+sorrel012@users.noreply.github.com>
2023-12-15 23:19:13 +0900

    [level unrated] Title: 커피 심부름, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181837. 커피 심부름/커피 심부름.js
+++ 프로그래머스/unrated/181837. 커피 심부름/커피 심부름.js
@@ -1,5 +1,5 @@
 function solution(order) {
     return order.reduce((total, value) => {
-        return value.indexOf('cafelatte') !== -1 ? total + 5000 : total + 4500;  
+        return total + (value.indexOf('cafelatte') !== -1 ?  5000 : 4500);  
     }, 0);
 }
