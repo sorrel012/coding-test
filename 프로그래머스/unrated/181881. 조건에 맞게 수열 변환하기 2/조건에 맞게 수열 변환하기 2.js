f8e5823a9778012bac077562fd3bc35438a8088c
Hana <115568532+sorrel012@users.noreply.github.com>
2023-12-17 20:32:06 +0900

    [level unrated] Title: 조건에 맞게 수열 변환하기 2, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181881. 조건에 맞게 수열 변환하기 2/조건에 맞게 수열 변환하기 2.js
+++ 프로그래머스/unrated/181881. 조건에 맞게 수열 변환하기 2/조건에 맞게 수열 변환하기 2.js
@@ -1,14 +1,18 @@
 function solution(arr) {
-    let cnt = 0;
-    let arr1 = arr;
-    let arr2 = [];
-    do {
-        arr2 = arr1.map(value => value >= 50 && value % 2 === 0 ? value / 2 : value < 50 && value % 2 !== 0 ? value * 2 + 1 : value);
-        if (JSON.stringify(arr1) === JSON.stringify(arr2)) {
-            break;
-        }
-        cnt++;
-        arr1 = arr2.slice();
-    } while (true);
-    return cnt;
+    let answer = 0;
+    let prev = [];
+
+    while(!arr.every((value,idx) => value === prev[idx])) {
+        prev =[...arr];
+
+        arr = arr.map(value => {
+            if(value >= 50 & value % 2 == 0)
+                return value / 2;
+            if(value < 50 & value % 2 != 0)
+                return value * 2 + 1;
+            return value;
+        })
+        answer++;
+    }
+    return answer - 1;
 }
