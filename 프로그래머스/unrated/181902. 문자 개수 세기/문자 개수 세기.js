a8bdc5b8ba46ffe9a810f6bb1279457ddf6c2aa2
Hana <115568532+sorrel012@users.noreply.github.com>
2023-12-18 10:41:05 +0900

    [level unrated] Title: 문자 개수 세기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181902. 문자 개수 세기/문자 개수 세기.js
+++ 프로그래머스/unrated/181902. 문자 개수 세기/문자 개수 세기.js
@@ -1,12 +1,3 @@
 function solution(my_string) {
-    let answer = [];
-    for(let i = 65; i <= 90; i++) {
-        answer.push(my_string.split(String.fromCodePoint(i)).length - 1);
-    }
-    
-    for(let i = 97; i <= 122; i++) {
-        answer.push(my_string.split(String.fromCodePoint(i)).length - 1);
-    }
-    
-    return answer;
+    return [...my_string].reduce((arr, char) => (arr[char.charCodeAt() - (char === char.toLowerCase() ? 71 : 65)]++, arr), Array(52).fill(0));
 }
