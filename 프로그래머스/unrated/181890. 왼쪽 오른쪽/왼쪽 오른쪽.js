97a9032c8e09d1eda96da34dfb5f6be14cf6881c
Hana <115568532+sorrel012@users.noreply.github.com>
2023-12-18 11:07:57 +0900

    [level unrated] Title: 왼쪽 오른쪽, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181890. 왼쪽 오른쪽/왼쪽 오른쪽.js
+++ 프로그래머스/unrated/181890. 왼쪽 오른쪽/왼쪽 오른쪽.js
@@ -1,12 +1,7 @@
-function solution(str_list) {
-    const idx1 = str_list.indexOf('l');
-    const idx2 = str_list.indexOf('r');
-
-    if (idx1 === -1 && idx2 === -1) {
-        return [];
-    } else if (idx1 !== -1 && (idx2 === -1 || idx1 < idx2)) {
-        return str_list.slice(0, idx1);
-    } else {
-        return str_list.slice(idx2 + 1);
+function solution(arr) {
+    for(let i = 0; i < arr.length; i++) {
+        if (arr[i] === 'l') return arr.slice(0, i);
+        if (arr[i] === 'r') return arr.slice(i + 1);
     }
+    return [];
 }
