877d4871b543039a04463a80b748d508f7ed2574
Hana <115568532+sorrel012@users.noreply.github.com>
2023-12-20 17:16:50 +0900

    [level unrated] Title: 등수 매기기, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/120882. 등수 매기기/등수 매기기.js
+++ 프로그래머스/unrated/120882. 등수 매기기/등수 매기기.js
@@ -1,23 +1,7 @@
-function solution(scores) {
-    // 각 학생의 평균 점수와 원래 인덱스를 계산
-    let averages = scores.map((score, index) => {
-        let avg = (score[0] + score[1]) / 2;
-        return { avg, index };
-    });
-
-    // 평균 점수를 기준으로 내림차순 정렬
-    averages.sort((a, b) => b.avg - a.avg);
-
-    // 등수 매기기
-    let ranks = Array(scores.length).fill(0);
-    let currentRank = 1;
-
-    for (let i = 0; i < averages.length; i++) {
-        if (i > 0 && averages[i].avg !== averages[i - 1].avg) {
-            currentRank = i + 1;
-        }
-        ranks[averages[i].index] = currentRank;
-    }
-
-    return ranks;
+function solution(score) {
+  return score.map((el) => {
+    return (
+      score.filter((v) => (v[0] + v[1]) / 2 > (el[0] + el[1]) / 2).length + 1
+    );
+  });
 }
