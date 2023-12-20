2fc32caf1166a375ea55aa4cdb0a4a72e3ebcfad
Hana <115568532+sorrel012@users.noreply.github.com>
2023-12-20 10:33:02 +0900

    [level unrated] Title: 조건 문자열, Time: 0.03 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181934. 조건 문자열/조건 문자열.js
@@ -0,0 +1,11 @@
+function solution(ineq, eq, n, m) {
+    if(eq === '=' && ineq === '<') {
+        return n <= m ? 1 : 0;
+    } else if(eq === '=' && ineq === '>') {
+        return n >= m ? 1 : 0;
+    } else if(eq === '!' && ineq === '<') {
+        return n < m ? 1 : 0;
+    } else {
+        return  n > m ? 1 : 0;
+    }
+}
