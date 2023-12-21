dea9fc40e157fecb011193231eb77d441c018d8f
Hana <115568532+sorrel012@users.noreply.github.com>
2023-12-21 09:51:07 +0900

    [level unrated] Title: 문자열 겹쳐쓰기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.js
+++ 프로그래머스/unrated/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.js
@@ -1,4 +1,3 @@
 function solution(my_string, overwrite_string, s) {
-    let arr = [...my_string];
-    return [...arr.slice(0, s), ...[...overwrite_string], ... arr.slice(overwrite_string.length + s)].join('');
+    return my_string.slice(0, s) + overwrite_string + my_string.slice(overwrite_string.length + s);
 }
