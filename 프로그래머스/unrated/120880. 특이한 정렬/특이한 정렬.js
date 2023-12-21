c2bfb54e81d10c30373e9f24499c621ff290de7f
Hana <115568532+sorrel012@users.noreply.github.com>
2023-12-21 10:02:25 +0900

    [level unrated] Title: 특이한 정렬, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/120880. 특이한 정렬/특이한 정렬.js
+++ 프로그래머스/unrated/120880. 특이한 정렬/특이한 정렬.js
@@ -1,8 +1,3 @@
 function solution(numlist, n) {
-    return numlist.map(value => [value, Math.abs(n - value)]).sort((a, b) => {
-        if (a[1] === b[1]) {
-            return b[0] - a[0];
-        }
-        return a[1] - b[1];
-    }).map(v => v[0]);
+  return numlist.sort((a, b) => Math.abs(a - n) - Math.abs(b - n) || b - a);
 }
