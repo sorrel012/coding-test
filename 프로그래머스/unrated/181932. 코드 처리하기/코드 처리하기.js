abdb07f351854b670c3d1be5c085583187793424
Hana <115568532+sorrel012@users.noreply.github.com>
2023-12-21 20:37:12 +0900

    [level unrated] Title: 코드 처리하기, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181932. 코드 처리하기/코드 처리하기.js
+++ 프로그래머스/unrated/181932. 코드 처리하기/코드 처리하기.js
@@ -1,18 +1,13 @@
 function solution(code) {
     let ret = [];
-    let mode = 0;
+    let mode = 0;    
     [...code].forEach((v, i) => {
-        if(mode === 0) {
-            if(v === '1') mode = 1;
-            else {
-                if (i % 2 === 0) ret.push(v);
-            }
+        if(v === '1') {
+            mode = mode === 0 ? 1 : 0;
         } else {
-            if(v === '1') mode = 0;
-            else {
-                if (i % 2 === 1) ret.push(v);
-            }
+            if(mode === 0 & i%2 === 0 || mode === 1 && i%2 === 1) ret.push(v);
         }
+        
     });
     return ret.join('') || 'EMPTY';
 }
