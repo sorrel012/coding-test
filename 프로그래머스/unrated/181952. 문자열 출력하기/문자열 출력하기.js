e712a2337a9f7a4fed86a7b7d9e463f1bc5021c1
Hana <115568532+sorrel012@users.noreply.github.com>
2023-12-21 19:09:01 +0900

    [level unrated] Title: 문자열 출력하기, Time: 44.81 ms, Memory: 32.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181952. 문자열 출력하기/문자열 출력하기.js
+++ 프로그래머스/unrated/181952. 문자열 출력하기/문자열 출력하기.js
@@ -2,10 +2,4 @@ const readline = require('readline');
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
-});
-
-let input = [];
-
-rl.on('line', function (line) {
-    console.log([...line].join(''));
-})
+}).on('line', console.log);
