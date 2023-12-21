d5418db0d5d18261402ad50f1c3a603d73c7f0f5
Hana <115568532+sorrel012@users.noreply.github.com>
2023-12-21 16:50:37 +0900

    [level unrated] Title: 다음에 올 숫자, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/120924. 다음에 올 숫자/다음에 올 숫자.js
+++ 프로그래머스/unrated/120924. 다음에 올 숫자/다음에 올 숫자.js
@@ -1,11 +1,8 @@
 function solution(common) {
-    let isArithmeticSequence = true;
-    const len = common.length;
-    for(let i = len-2; i > 0; i--) {
-        if(common[i+1] - common[i] !== common[i] - common[i-1]) {
-            isArithmeticSequence = false;
-            break;
-        }
+    if ((common[1]-common[0]) === (common[2]-common[1])){
+        return common.pop() + (common[1] - common[0]);
     }
-    return isArithmeticSequence ? common[len-1] + (common[1] - common[0]) : common[len-1] * (common[1] / common[0])
+    else{
+        return common.pop() * (common[1] / common[0]);
+    }
 }
