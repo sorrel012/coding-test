feb2f318ff1aa40ad066f31ffd597d5a268b0445
Hana <115568532+sorrel012@users.noreply.github.com>
2023-12-22 14:23:37 +0900

    [level unrated] Title: 겹치는 선분의 길이, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/120876. 겹치는 선분의 길이/겹치는 선분의 길이.js
+++ 프로그래머스/unrated/120876. 겹치는 선분의 길이/겹치는 선분의 길이.js
@@ -1,27 +1,9 @@
 function solution(lines) {
-    let points = [];
-    lines.forEach(line => {
-        points.push({ pos: line[0], type: 'start' });
-        points.push({ pos: line[1], type: 'end' });
+    let line = new Array(201).fill(0);
+
+    lines.forEach(([begin, end]) => {
+        for(let i = begin; i < end; i++) line[i + 100]++;
     });
 
-    points.sort((a, b) => a.pos - b.pos);
-
-    let overlapLength = 0; 
-    let activeSegments = 0; 
-    let lastPos = points[0].pos; 
-
-    points.forEach(point => {
-        if (activeSegments >= 2) {
-            overlapLength += point.pos - lastPos;
-        }
-        if (point.type === 'start') {
-            activeSegments++;
-        } else {
-            activeSegments--;
-        }
-        lastPos = point.pos;
-    });
-
-    return overlapLength;
+    return line.reduce((sum, v) =>  v > 1 ? sum+1 : sum, 0)
 }
