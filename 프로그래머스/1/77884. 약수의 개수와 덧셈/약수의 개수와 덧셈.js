e300028612b85bba5bd6f998ff8676e81bd1574a
Hana <115568532+sorrel012@users.noreply.github.com>
2023-12-27 17:18:55 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.62 ms, Memory: 35.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
@@ -0,0 +1,18 @@
+function countDivisor(num) {
+    let cnt = 0;
+    for(let i = 1; i <= num; i++) {
+        if(num % i === 0) cnt++;
+    }
+    return cnt % 2 === 0;
+}
+function solution(left, right) {
+    let answer = 0;
+    for(let i = left; i <= right; i++) {
+        if(countDivisor(i)) {
+            answer += i;
+        } else {
+            answer -= i;
+        }
+    }
+    return answer;
+}
