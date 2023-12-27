3e0e3c3d956e090d67e971f1ba327821a777ff9a
Hana <115568532+sorrel012@users.noreply.github.com>
2023-12-27 11:08:19 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -0,0 +1,11 @@
+function solution(num) {
+    let cnt = 0;
+    while(num !== 1) {
+        if(cnt > 500) {
+            return -1;
+        }
+        num = (num%2 === 0) ? num / 2 : num * 3 + 1;
+        cnt++;
+    }
+    return cnt;
+}
