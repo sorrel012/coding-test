5d2c3c0b67b44e7c21497ffd9373d76c26237f74
Hana <115568532+sorrel012@users.noreply.github.com>
2023-12-28 10:46:15 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
@@ -1,11 +1,4 @@
 function solution(n) {
-    let answer = '';
-    for(let i = 1; i <= n; i++) {
-        if(i % 2 === 1) {
-            answer += '수';
-        } else {
-            answer += '박';
-        }
-    }
-    return answer;
+    const waterMelon = n =>'수박'.repeat(n/2) + (n%2 === 1 ? '수' : '');
+    return waterMelon(n);
 }
