6ff2028d3ffa1926aff24036eb69990f6f7eeef4
Hana <115568532+sorrel012@users.noreply.github.com>
2023-12-29 10:19:42 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 25.40 ms, Memory: 83.2 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -1,9 +1,3 @@
 function solution(arr) {
-    let answer = [];
-    for(let i = 0; i < arr.length; i++) {
-        if(arr[i] !== arr[i-1]) {
-            answer.push(arr[i]);
-        }
-    }
-    return answer;
+    return arr.filter((v,i) => v != arr[i+1]);
 }
