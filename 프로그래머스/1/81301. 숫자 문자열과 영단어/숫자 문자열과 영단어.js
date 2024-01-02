a5f7f1acde2747a2eae1509f7ba11519a732b012
Hana <115568532+sorrel012@users.noreply.github.com>
2024-01-02 10:52:11 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.10 ms, Memory: 33.2 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -1,21 +1,11 @@
-function solution(s) {
-    const num = {
-        zero: 0,
-        one: 1,
-        two: 2,
-        three: 3,
-        four: 4,
-        five: 5,
-        six: 6,
-        seven: 7,
-        eight: 8,
-        nine: 9        
-    }
-    
-    Object.keys(num).forEach(key => {
-        const regExp = new RegExp(key, 'g');
-        s = s.replace(regExp, num[key]);
-    });
+function solution(s) {    
+    const numbers = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+    let answer = s;
 
-    return parseInt(s);
+    for(let i = 0; i < numbers.length; i++) {
+        let arr = answer.split(numbers[i]);
+        answer = arr.join(i);
+    }
+
+    return Number(answer);
 }
