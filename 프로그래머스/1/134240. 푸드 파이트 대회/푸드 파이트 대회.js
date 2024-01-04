4858a79a5e681e9bee4932c126d3b44d6f4c6135
Hana <115568532+sorrel012@users.noreply.github.com>
2024-01-04 10:51:22 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.js
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.js
@@ -1,15 +1,8 @@
 function solution(food) {
-    let answer = [];
-    food.shift();
-    
-    food.forEach((el, i) => {
-        for(let j = 0; j < Math.floor(el/2); j++) {
-            answer.push(i+1);
-        }
-    })
-    answer.push(0);
-    for(let k = answer.length - 2; k >= 0; k--) {
-        answer.push(answer[k]);
+    let res = '';
+    for (let i = 1; i < food.length; i++) {
+        res += String(i).repeat(Math.floor(food[i]/2));
     }
-    return answer.join('');
+
+    return res + '0' + [...res].reverse().join('');
 }
