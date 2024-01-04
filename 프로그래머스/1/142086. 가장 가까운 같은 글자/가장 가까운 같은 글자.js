f14b1a40d2606b61398f1a4171b94061f94a9955
Hana <115568532+sorrel012@users.noreply.github.com>
2024-01-04 11:17:43 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 2.04 ms, Memory: 36.8 MB -BaekjoonHub

--- 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
@@ -1,12 +1,6 @@
 function solution(s) {
-    let answer = new Array(s.length).fill(-1);
-    for(let i = 1; i < s.length; i++) {
-        for(let j = i-1; j >= 0; j--) {
-            if(s[i] === s[j]) {
-                answer[i] = i - j;
-                break;
-            }
-        }        
-    }
-    return answer;
+  return [...s].map((char, i) => {
+    const count = s.slice(0, i).lastIndexOf(char);
+    return count < 0 ? count : i - count;
+  });
 }
