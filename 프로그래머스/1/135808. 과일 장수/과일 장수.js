ea45f867e3bc5263986a4ae4310b86d78255d081
Hana <115568532+sorrel012@users.noreply.github.com>
2024-01-09 17:52:10 +0900

    [level 1] Title: 과일 장수, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.js
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.js
@@ -1,8 +1,14 @@
 function solution(k, m, score) {
-    let answer = 0;
-    score.sort((a, b) => b - a);
-    for(let i = 0; i < score.length-m+1; i += m) {
-        answer += Math.min(...score.slice(i, i+m)) * m;
+    let maxProfit = 0;
+    score.sort((a, b) => b - a); 
+
+    for(let i = 0; i <= score.length - m; i += m) {
+        let minScoreInBox = score[i];
+        for (let j = 1; j < m; j++) {
+            minScoreInBox = Math.min(minScoreInBox, score[i + j]);
+        }
+        maxProfit += minScoreInBox * m;
     }
-    return answer;
+
+    return maxProfit;
 }
