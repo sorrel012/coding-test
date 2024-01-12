df0bfd66112e3d59e5fda305124076fbcf3714dc
Hana <115568532+sorrel012@users.noreply.github.com>
2024-01-12 13:37:53 +0900

    [level Lv.1] Title: 로또의 최고 순위와 최저 순위, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/Lv.1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.js
+++ 프로그래머스/Lv.1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.js
@@ -1,15 +1,16 @@
 function solution(lottos, win_nums) {
-    const same = lottos.filter(v => win_nums.includes(v));
-    const len = same.length;
-    if(len === 6) {
-        return [1, 1];
+    const winNumsSet = new Set(win_nums);
+    let matchCount = 0;
+    let zerosCount = 0;
+
+    for (const num of lottos) {
+        if (num === 0) {
+            zerosCount++;
+        } else if (winNumsSet.has(num)) {
+            matchCount++;
+        }
     }
-    same.forEach(v => {
-        win_nums.splice(win_nums.indexOf(v), 1);
-    })
-    let zeroNums = lottos.filter(v => v === 0).length;
     
-    const rank = [6, 6, 5, 4, 3, 2, 1];
-    
-    return [rank[len + zeroNums], rank[len]];
+    const rank = [6, 6, 5, 4, 3, 2, 1];    
+    return [rank[matchCount + zerosCount], rank[matchCount]];
 }
