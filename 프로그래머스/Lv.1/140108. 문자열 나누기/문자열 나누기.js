5bc107b238b3e0e4491bf65ae39b7f0aeb025663
Hana <115568532+sorrel012@users.noreply.github.com>
2024-01-15 15:16:19 +0900

    [level Lv.1] Title: 문자열 나누기, Time: 0.60 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/Lv.1/140108. 문자열 나누기/문자열 나누기.js
+++ 프로그래머스/Lv.1/140108. 문자열 나누기/문자열 나누기.js
@@ -2,19 +2,19 @@ function solution(s) {
     let firstCnt = 0;
     let othersCnt = 0;
     
-    let answer = [];
+    let answer = 0;
     let startIdx = 0;
     for (let i = 0; i < s.length; i++) {
         s[i] === s[startIdx] ? firstCnt++ : othersCnt++;
         if (firstCnt === othersCnt) {
-            answer.push(s.substring(startIdx, i + 1));
+            answer++;
             startIdx = i + 1;
         }
     }
 
     if (startIdx < s.length) {
-        answer.push(s.substring(startIdx));
+        answer++;
     }
 
-    return answer.length;
+    return answer;
 }
