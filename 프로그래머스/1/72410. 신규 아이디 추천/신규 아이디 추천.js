f4953fab8b408200d576996d76fdf6aef9a8b916
Hana <115568532+sorrel012@users.noreply.github.com>
2024-01-26 10:27:40 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.17 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.js
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.js
@@ -1,22 +1,12 @@
 function solution(new_id) {
-    new_id = new_id.toLowerCase()
+    const answer = new_id.toLowerCase()
                     .replace(/[^a-z0-9-_.]/g, '')
                     .replace(/\.{2,}/g, '.')
-                    .replace(/^/, '')
                     .replace(/^\./, '')
-                    .replace(/\.$/, '');
+                    .replace(/\.$/, '')
+                    .replace(/^$/, 'a')
+                    .slice(0, 15).replace(/\.$/, '');
     
-    if(new_id === '') {
-        new_id = 'a';
-    }
-    
-    if(new_id.length >= 16) {
-        new_id = new_id.slice(0, 15).replace(/\.$/, '');
-    }
-    
-    while(new_id.length <= 2) {
-        new_id += new_id[new_id.length - 1];
-    }
-    
-    return new_id;    
+    const len = answer.length;
+    return len > 2 ? answer : answer + answer.charAt(len - 1).repeat(3 - len);
 }
