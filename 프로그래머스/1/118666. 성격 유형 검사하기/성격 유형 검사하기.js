a9eaefdb375f5e9c21aec60f2fa0da99f9094ea6
Hana <115568532+sorrel012@users.noreply.github.com>
2024-01-29 11:01:06 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.50 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.js
+++ 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.js
@@ -1,64 +1,16 @@
 function solution(survey, choices) {
-    const scores = {
-        1: 3,
-        2: 2,
-        3: 1,
-        4: 0,
-        5: 1,
-        6: 2,
-        7: 3,
-    }
-    
-    let result = {
-        'R': 0,
-        'T': 0,
-        'C': 0,
-        'F': 0,
-        'J': 0,
-        'M': 0,
-        'A': 0,
-        'N': 0,
-    }
-    
-    choices.forEach((v, i) => {
-        let question = survey[i];
-        let plus ='';
-        if(v < 4) { 
-            plus = question.charAt(0);
-        } else {            
-            plus = question.charAt(1);
-        }
-        result[plus] += scores[v];        
-    })
-    
-    let answer = ''
-    
-    if(result['R'] >= result['T']) {
-        answer += 'R'
-    } else  {
-        answer += 'T'
-    }
-    
-    
-    if(result['C'] >= result['F']) {
-        answer += 'C'
-    } else  {
-        answer += 'F'
-    }
-    
-    
-    if(result['J'] >= result['M']) {
-        answer += 'J'
-    } else  {
-        answer += 'M'
-    }
-    
-    
-    if(result['A'] >= result['N']) {
-        answer += 'A'
-    } else  {
-        answer += 'N'
-    }
-    
-    return answer;
+    const MBTI = {};
+    const types = ['RT', 'CF', 'JM', 'AN'];
+
+    types.forEach((type) =>
+        type.split('').forEach((char) => MBTI[char] = 0)
+    )
+
+    choices.forEach((choice, index) => {
+        const [disagree, agree] = survey[index];
+
+        MBTI[choice > 4 ? agree : disagree] += Math.abs(choice - 4);
+    });
+
+    return types.map(([a, b]) => MBTI[b] > MBTI[a] ? b : a).join('');
 }
