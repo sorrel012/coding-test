e99a6401edb1e86c9f365195c5a213e631ba6553
Hana <115568532+sorrel012@users.noreply.github.com>
2024-01-30 13:24:12 +0900

    [level 1] Title: 바탕화면 정리, Time: 0.37 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.js
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.js
@@ -1,20 +1,14 @@
 function solution(wallpaper) {
-    let position = [];
+    let positionX = []; //column
+    let positionY = []; //row
     wallpaper.forEach((row, i) => {
         [...row].forEach((el, j) => {
             if(el === '#') {
-                position.push([i, j]);
+                positionX.push(i);
+                positionY.push(j);
             }
         })
     })
     
-    const sortX = position.sort((a, b) => a[0] - b[0]);
-    const lux = sortX[0][0];
-    const rdx = sortX[position.length-1][0] + 1;
-    
-    const sortY = position.sort((a, b) => a[1] - b[1]);
-    const luy = sortX[0][1];
-    const rdy = sortX[position.length-1][1] + 1;
-    
-    return [lux, luy, rdx, rdy];
+    return [Math.min(...positionX), Math.min(...positionY), Math.max(...positionX)+1, Math.max(...positionY)+1];
 }
