a2efaf5306f2d2a2c2cf54d3cb8509b54f8b8816
Hana <115568532+sorrel012@users.noreply.github.com>
2024-02-19 10:54:10 +0900

    [level 2] Title: 숫자의 표현, Time: 0.14 ms, Memory: 33.2 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.js
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.js
@@ -1,7 +1,9 @@
 function solution(n) {
     let answer = 0;
     for(let k = 1; k*(k+1)/2 <= n; k++) {
-        if ((n - k*(k+1)/2) % k == 0) answer++;
+        if ((n - k*(k+1)/2) % k === 0) {
+            answer++; 
+        }
     }
     return answer;
 }
