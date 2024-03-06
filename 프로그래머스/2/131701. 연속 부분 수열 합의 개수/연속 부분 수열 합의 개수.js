f21152763e64fff736ed1efd80aef74f1a803a03
Hana <115568532+sorrel012@users.noreply.github.com>
2024-03-06 10:12:33 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 92.85 ms, Memory: 56.9 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
@@ -1,18 +1,13 @@
 function solution(elements) {
-    const doubledElements = [...elements, ...elements];
-    const sumSet = new Set(); 
-
-    const n = elements.length;
-
-    for (let length = 1; length <= n; length++) {
-        for (let start = 0; start < n; start++) {
-            let sum = 0;
-            for (let i = start; i < start+length; i++) {
-                sum += doubledElements[i];
-            }
-            sumSet.add(sum);
+    const circular = elements.concat(elements);
+    const sumSet = new Set();
+    
+    for (let left = 0; left < elements.length; left++) {
+        let sum = 0;
+        for (let right = 0; right < elements.length; right++) {
+            sum += circular[left + right];
+            sumSet.add(sum);            
         }
     }
-
     return sumSet.size;
 }
