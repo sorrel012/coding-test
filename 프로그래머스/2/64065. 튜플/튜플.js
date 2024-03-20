2619ace5c6ed7f333a95e739bf6e039cb044d47f
Hana <115568532+sorrel012@users.noreply.github.com>
2024-03-20 09:59:50 +0900

    [level 2] Title: 튜플, Time: 0.12 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.js
+++ 프로그래머스/2/64065. 튜플/튜플.js
@@ -1,17 +1,8 @@
 function solution(s) {
-    let answer = [];
-    let arr = s.match(/{(.*?)}/g).map(s =>
-        s.replace(/[{}]/g, '').split(',').map(Number)
-    );
+    let arr = s.slice(2, -2).split('},{').map(str => str.split(',').map(Number));
     
     arr.sort((a, b) => a.length - b.length);
     
-    return arr.reduce((acc, curr, i) => {
-        
-        curr.forEach(num => {
-            if(acc.indexOf(num) === -1)  acc.push(num)
-        })
-        
-        return acc;
-    }, []);
+    return arr.reduce((acc, cur) => [...acc, ...cur.filter(num => !acc.includes(num))], []);
 }
+
