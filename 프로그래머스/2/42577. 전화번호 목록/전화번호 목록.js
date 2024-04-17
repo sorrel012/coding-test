2c01db264d347632b54f5e95459da8539573193c
Hana <115568532+sorrel012@users.noreply.github.com>
2024-04-17 11:00:52 +0900

    [level 2] Title: 전화번호 목록, Time: 114.76 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
@@ -1,10 +1,6 @@
 function solution(phone_book) {
-    phone_book.sort();
-    
-    for (let i = 0; i < phone_book.length - 1; i++) {
-        if (phone_book[i + 1].startsWith(phone_book[i])) {
-            return false;
-        }
-    }
-    return true;
+    return !phone_book.sort().some((_,i)=> {
+        if(i === phone_book.length -1) return false;
+        return phone_book[i+1].startsWith(phone_book[i]);        
+    })
 }
