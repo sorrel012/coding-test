ddf35c2e7e935130072978e7dbb17273f031aed8
Hana <115568532+sorrel012@users.noreply.github.com>
2024-05-08 10:56:43 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.js
+++ 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.js
@@ -1,30 +1,12 @@
-function isPrime(number) {
-    if (number <= 1) return false;
-    if (number <= 3) return true;
-    if (number % 2 === 0 || number % 3 === 0) return false;
-    for (let i = 5; i * i <= number; i += 6) {
-        if (number % i === 0 || number % (i + 2) === 0) return false;
+function isPrime(num){
+    if(!num || num === 1) return false;
+    for(let i = 2; i <= +Math.sqrt(num); i++){
+        if(num % i===0) return false;
     }
     return true;
 }
 
-function solution(n, k) {
-    let answer = 0
-    let convertedNum = n.toString(k);
-    
-    for(let i = 0; i < convertedNum.length; i++) {
-        let tmp = '';
-        for(let j = i; j < convertedNum.length; j++) {
-            if(convertedNum[j] === '0') {
-                break;
-            }
-            tmp += convertedNum[j];
-            i = j;
-        }
-        
-        if(isPrime(+tmp)) answer++;
-        
-    }
-    
-    return answer;
+function solution(n, k) {    
+    const candidates = n.toString(k).split('0');
+    return candidates.filter(v => isPrime(+v)).length;
 }
