8e93fd3bafcdfa156da43fa064cc9bce97f367d0
Hana <115568532+sorrel012@users.noreply.github.com>
2024-05-10 13:28:02 +0900

    [level 2] Title: [3차] 압축, Time: 113.41 ms, Memory: 49.9 MB -BaekjoonHub

--- 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.js
+++ 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.js
@@ -1,33 +1,22 @@
 function solution(msg) {
-    let answer = [];
-    let dictionary = {};
+    const dict = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split('').reduce((dict, curr, i) => {
+        dict[curr] = i + 1;
+        return dict;
+    }, {});
+    dict.newIndex = 27;
     
-    for (let i = 0; i < 26; i++) {
-        dictionary[String.fromCharCode(i + 65)] = i + 1;
-    }
-    
-    let nextIndex = 27;
-    let index = 0;
-
-    while (index < msg.length) {
-        let w = '';
+    const answer = [];
+    for(let i = 0, j = 0; i < msg.length; i = j){
+        j = msg.length;
         
-        for (let len = 1; index + len - 1 < msg.length; len++) {
-            let current = msg.substring(index, index + len);
-            if (dictionary[current] !== undefined) {
-                w = current;
-            } else {
-                break;
-            }
+        let longest = '';        
+        while(dict[(longest = msg.substring(i, j))] === undefined) --j;
+        
+        answer.push(dict[longest]);
+        
+        if (j < msg.length) { 
+            dict[longest + msg[j]] = dict.newIndex++;
         }
-        answer.push(dictionary[w]);
-        
-        if (index + w.length < msg.length) {
-            let wc = w + msg[index + w.length];
-            dictionary[wc] = nextIndex++;
-        }
-        
-        index += w.length;
     }
 
     return answer;
