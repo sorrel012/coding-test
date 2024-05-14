8c440c8a99fc8fe6914116580959138bdeb2ba89
Hana <115568532+sorrel012@users.noreply.github.com>
2024-05-14 09:54:42 +0900

    [level 2] Title: 주식가격, Time: 26.16 ms, Memory: 41.8 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.js
+++ 프로그래머스/2/42584. 주식가격/주식가격.js
@@ -1,14 +1,20 @@
 function solution(prices) {
-    let answer = [];
-    
-    for(let i = 0; i < prices.length; i++) {
-        let period = 0;
-        for(let j = i+1; j < prices.length; j++) {
-            period++;
-            if(prices[i] > prices[j]) break;
-        }
-        answer.push(period);
+  const answer = new Array(prices.length).fill(0);
+  const stack = [];
+  let priceLen = prices.length;
+
+  for(let i = 0; i < priceLen; i++) {
+    while(stack.length && prices[i] < prices[stack[stack.length - 1]]) {
+      let tmp = stack.pop();
+      answer[tmp] = i - tmp;
     }
-    
-    return answer;
+    stack.push(i);
+  }
+
+  while(stack.length) {
+    let tmp = stack.pop();
+    answer[tmp] = priceLen - tmp - 1;
+  }
+
+  return answer;
 }
