cbbbee9ff5e124e7006bdd94cfa24f7a2bfe70a6
Hana <115568532+sorrel012@users.noreply.github.com>
2024-05-16 17:14:20 +0900

    [level 2] Title: 더 맵게, Time: 413.44 ms, Memory: 110 MB -BaekjoonHub

---
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.js
@@ -0,0 +1,38 @@
+function solution(scoville, K) {
+    let answer = 0;
+    let newScoville = [];
+
+    scoville.sort((a,b)=>a-b);
+
+    let left = 0; 
+    let right = 0;
+
+    while((scoville.length-left+newScoville.length-right) >= 2 && (scoville[left] < K || newScoville[right] < K)) {
+        let origin1;
+        let origin2;
+        let new1;
+        let new2;
+
+        if(left < scoville.length) origin1 = scoville[left];
+        if(left+1 < scoville.length) origin2 = scoville[left+1];
+        if(right < newScoville.length) new1 = newScoville[right];
+        if(right+1 < newScoville.length) new2 = newScoville[right+1];
+
+
+        if(newScoville.length===0 || right >= newScoville.length || (origin2!==undefined && origin2 <= new1)) {
+            left += 2;
+            newScoville.push(origin1+origin2*2);
+        } else if(left >= scoville.length || new2!==undefined && new2 <= origin1 ) {
+            right += 2;
+            newScoville.push(new1+new2*2);
+        } else {
+            left += 1;
+            right += 1;
+            origin1 < new1 ? newScoville.push(origin1+new1*2):newScoville.push(new1+origin1*2);
+        }
+
+        answer += 1;
+    }
+
+    return (scoville[left] < K || newScoville[right] < K) ? -1 : answer;
+}
