5f999f1dccdf5fd91ec0f2dbe66a15af3853a58a
Hana <115568532+sorrel012@users.noreply.github.com>
2024-05-22 22:38:32 +0900

    [level 2] Title: 땅따먹기, Time: 13.92 ms, Memory: 72.9 MB -BaekjoonHub

--- 프로그래머스/2/12913. 땅따먹기/땅따먹기.js
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.js
@@ -1,9 +1,10 @@
 function solution(land) {
-    for (let i = 1; i < land.length; i++) {
-        land[i][0] += Math.max(land[i-1][1], land[i-1][2], land[i-1][3]);
-        land[i][1] += Math.max(land[i-1][0], land[i-1][2], land[i-1][3]);
-        land[i][2] += Math.max(land[i-1][0], land[i-1][1], land[i-1][3]);
-        land[i][3] += Math.max(land[i-1][0], land[i-1][1], land[i-1][2]);
-    }
-    return Math.max(...land[land.length-1]);
+    return Math.max(...land.reduce((acc, curr) => {
+        return [
+            curr[0] + Math.max(acc[1], acc[2], acc[3]),  
+            curr[1] + Math.max(acc[0], acc[2], acc[3]),
+            curr[2] + Math.max(acc[0], acc[1], acc[3]),
+            curr[3] + Math.max(acc[0], acc[1], acc[2]),
+        ];
+    }, [0, 0, 0, 0]));
 }
