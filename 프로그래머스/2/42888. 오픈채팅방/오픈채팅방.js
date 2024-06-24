291124b2dc09b8fa1b5e3a5b1e8b923f28ef5006
Hana <115568532+sorrel012@users.noreply.github.com>
2024-06-24 18:01:16 +0900

    [level 2] Title: 오픈채팅방, Time: 163.44 ms, Memory: 98.1 MB -BaekjoonHub

--- 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.js
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.js
@@ -1,22 +1,27 @@
 function solution(records) {
     const answer = [];
     const user = new Map();
+    const typeMapping = {
+        'Enter': '님이 들어왔습니다.',
+        'Leave': '님이 나갔습니다.'
+    }
     
     for(const record of records) {  
-        const [type, uid, nickname] = record.split(' ');        
-        if (type === 'Enter' || type === 'Change') {
-            user.set(uid, nickname);
+        const [type, uid, nickname] = record.split(' ');      
+        
+        if (type !== 'Change') {
+            answer.push([type, uid]);
+        }
+        if(nickname) {
+            user.set(uid, nickname);            
         }
     }
     
-    for (const record of records) {
-        const [type, uid] = record.split(' ');        
-        if (type === 'Enter') {
-            answer.push(`${user.get(uid)}님이 들어왔습니다.`);
-        } else if (type === 'Leave') {
-            answer.push(`${user.get(uid)}님이 나갔습니다.`);
-        }
-    }
-    
-    return answer;
+    return answer.map(([type, uid]) => {
+        return `${user.get(uid)}${typeMapping[type]}`;    
+    })
 }
+
+
+
+    
