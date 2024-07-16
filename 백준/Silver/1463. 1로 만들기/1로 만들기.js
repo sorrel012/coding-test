26d047400f7d68ae9004418c368758c9490e999f
Hana <115568532+sorrel012@users.noreply.github.com>
2024-07-16 16:06:04 +0900

    [Silver III] Title: 1로 만들기, Time: 240 ms, Memory: 17764 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.js
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.js
@@ -14,10 +14,10 @@ rl.on('line', (line) => {
     for (let i = 2; i <= n; i++) {
         dp[i] = dp[i-1] + 1;
         if (i % 2 === 0) {
-            dp[i] = Math.min(dp[i], dp[i / 2] + 1);
+            dp[i] = Math.min(dp[i], dp[i/2] + 1);
         }
         if (i % 3 === 0) {
-            dp[i] = Math.min(dp[i], dp[i / 3] + 1);
+            dp[i] = Math.min(dp[i], dp[i/3] + 1);
         }
     }
 
