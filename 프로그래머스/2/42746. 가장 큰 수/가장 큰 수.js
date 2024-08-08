eb456a014209dd9317fc7b8cf04abce7dc0a647b
Hana <115568532+sorrel012@users.noreply.github.com>
2024-08-08 09:58:42 +0900

    [level 2] Title: 가장 큰 수, Time: 520.33 ms, Memory: 37.3 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.js
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.js
@@ -1,5 +1,5 @@
 function solution(numbers) {
-    const sortedNumbers = numbers.map(String).sort((a, b) => (b + a) - (a + b));
+    const sortedNumbers = numbers.map(String).sort((a, b) => (b+a) - (a+b));
     const result = sortedNumbers.join('');
     
     return result[0] === '0' ? '0' : result;
